9377547b1a2ef68d2ba9b3fe5396c9c86c40f415
LiteTekno92 <lite.tekno92@gmail.com>
2019-04-04 11:59:51 +0200

    All added

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:mock/drawer.dart';
+//import 'package:mock/MessageList.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      child: Scaffold(
+          appBar: AppBar(
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.refresh),
+                onPressed: () async {},
+              )
+            ],
+            bottom: TabBar(
+              tabs: <Widget>[
+                Tab(text: "Important"),
+                Tab(text: "Other"),
+              ],
+            ),
+          ),
+          drawer: AppDrawer(),
+          body: TabBarView(
+            children: <Widget>[
+              Text( "important"),
+              Text( "other"),
+            ],
+          )),
+      length: 2,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:emailapp/App.dart';
+import 'package:emailapp/MessageList.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(EmailApp());
+
+class EmailApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blueGrey, accentColor: Colors.red),
+      home: App(),
+    );
+  }
+}
